1187929c9fb168ecbd40f6342eb5cbe6d780f823
alvinlienardi <lienardialvinsebastian@gmail.com>
2025-05-24 15:59:27 +0900

    Develop UI

--- static/index.js
+++ static/index.js
@@ -4,6 +4,11 @@ $(document).ready(function () {
       const promptInput = $("#promptInput").val().trim();
       if (!promptInput) return;
 
+      $("#response").append(`<p class="user-input chat-message">${promptInput}</p>`);
+      $("#promptInput").val('');
+
+      addLoadingSpinner();
+
       $.ajax({
         url: '/ask',
         type: 'POST',
@@ -11,17 +16,33 @@ $(document).ready(function () {
         data: JSON.stringify({ prompt: promptInput }),
         success: function (data) {
           // const responseHtml = `<div class="alert alert-success mt-3" role="alert">${data.response}</div>`;
-          const responseHtml = `<p>${data.response}</p>`;
+          removeLoadingSpinner();
+          const responseHtml = `<p class="api-response chat-message">${data.response}</p>`;
           $("#response").append(responseHtml);
+          scrollDown();
         },
         error: function (xhr, status, error) {
           // const errorHtml = `<div class="alert alert-danger mt-3" role="alert">Something went wrong: ${error}</div>`;
-          const errorHtml = `<p>Something went wrong: ${error}</p>`;
+          removeLoadingSpinner();
+          const errorHtml = `<p class="api-response chat-message">Something went wrong: ${error}</p>`;
           $("#response").append(errorHtml);
+          scrollDown();
         }
       });
-
-      $("#promptInput").val('');
     }
   });
 });
+
+function scrollDown(){
+  $("#response").scrollTop($("#response")[0].scrollHeight);
+}
+
+function addLoadingSpinner(){
+  let spinner = '<div class="spinner-grow" role="status" id="loadingSpinner"><span class="visually-hidden">Loading...</span></div>';
+  $("#response").append(spinner);
+  scrollDown();
+}
+
+function removeLoadingSpinner(){
+  $("#loadingSpinner").remove();
+}
